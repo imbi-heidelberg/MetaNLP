2a04bfa885d0f4223c6d4f90cd269ee06944dc81
unknown <brudernico@gmail.com>
2024-04-11 18:00:45 +0200

    tests for delete_words

---
+++ tests/testthat/test_deletion.R
@@ -0,0 +1,21 @@
+test_that("Deletion functions work", {
+
+  # The first four words of the vector exist in (possibly modified) form in the
+  # word count matrix of the test data
+  obj <- MetaNLP("data/test_data.csv")
+  deletion_list <- c("beautiful", "considering", "facts", "find", "algebra")
+  obj_delete <- delete_words(obj, deletion_list)
+
+  # the data frame in obj_delete should have 4 columns less
+  expect_true(
+    ncol(obj@data_frame) - 4 == ncol(obj_delete@data_frame)
+  )
+
+  # check that the correct columns have been deleted
+  expect_true(
+    !("beauti" %in% names(obj_delete) |
+      "consid" %in% names(obj_delete) |
+      "fact"   %in% names(obj_delete) |
+      "find"   %in% names(obj_delete))
+  )
+})
