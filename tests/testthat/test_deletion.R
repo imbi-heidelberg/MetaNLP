e0af5bb5dcb1e9365225c377d9300885f9ae5714
unknown <brudernico@gmail.com>
2024-04-12 12:51:19 +0200

    added tests for delete_stop_words

--- tests/testthat/test_deletion.R
+++ tests/testthat/test_deletion.R
@@ -5,6 +5,17 @@ test_that("Deletion functions work", {
   obj <- MetaNLP("data/test_data.csv")
   deletion_list <- c("beautiful", "considering", "facts", "find", "algebra")
   obj_delete <- delete_words(obj, deletion_list)
+  obj_stop_words <- delete_stop_words(obj)
+
+  # delete_words and delete_stop_words return MetaNLP objects
+
+  expect_true(
+    isClass(obj_delete, MetaNLP)
+  )
+
+  expect_true(
+    isClass(obj_stop_words, MetaNLP)
+  )
 
   # the data frame in obj_delete should have 4 columns less
   expect_true(
@@ -17,5 +28,14 @@ test_that("Deletion functions work", {
       "consid" %in% names(obj_delete) |
       "fact"   %in% names(obj_delete) |
       "find"   %in% names(obj_delete))
+  )
+
+  # check that stop words have been deleted
+  expect_false(
+    !(any(tm::stopwords() %in% names(obj@data_frame)))
+  )
+
+  expect_true(
+    !(any(tm::stopwords() %in% names(obj_stop_words@data_frame)))
   )
 })
