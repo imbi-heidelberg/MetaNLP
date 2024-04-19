eccd90d5124ad27ce968fb65869a1b2e6a59c8e7
unknown <brudernico@gmail.com>
2024-04-19 14:54:21 +0200

    added function to write word count matrix

--- R/useful_functions.R
+++ R/useful_functions.R
@@ -75,3 +75,71 @@ setMethod("summary", signature("MetaNLP"),
                                            "Relative" = rel_include)))
           })
 
+
+#' @rdname write_csv
+#' @export
+setGeneric("write_csv", function(object, ...) {
+  standardGeneric("write_csv")
+})
+
+
+#' Save the word count matrix
+#'
+#' This function can be used to save the word count matrix of a MetaNLP object
+#' as a csv-file.
+#'
+#' @param object An object of class MetaNLP.
+#' @param path Path where to save the csv. If no path is set, the csv is saved
+#' in the current working directory
+#' @param type Specifies if the word count matrix should be saved as
+#' "train_wcm.csv" or "test_wcm.csv". If the user wants to use another file name,
+#' the whole path including the file name should be given as the \code{path}
+#' argument
+#' @param ... Additional arguments for \link[utils]{write.table}.
+#'
+#' @details
+#' Overall, there are three options to specify the path. By
+#' default, no path is set, so the csv is saved as "train_wcm.csv" or
+#' "test_wcm.csv" in the current working directory. If a path to a specific
+#' folder is given (but the path name does not end with ".csv"), the file is
+#' saved in this folder as "train_wcm.csv" or "test_wcm.csv".
+#' By providing a path ending with ".csv", the user can override the default
+#' naming convention and the file is saved according to this path.
+#'
+#' @examples
+#' \dontrun{
+#' obj <- MetaNLP("test_data.csv")
+#' obj2 <- delete_stop_words(obj)
+#' write_csv(obj2)
+#' write_csv(obj2, path = "foo.csv")
+#' }
+#'
+#'
+#' @rdname write_csv
+#' @export
+setMethod("write_csv", signature("MetaNLP"),
+          function(object, path = "", type = c("train", "test"), ...) {
+            lastchar <- 0
+
+            # extract data
+            data <- object@data_frame
+
+            # extract last 3 characters from path
+            if(nchar(path) > 3) {
+              lastchar <- substr(path, nchar(path) - 3, nchar(path))
+            }
+
+            # create file path
+            if(lastchar == ".csv") {
+              path_to_save <- path
+            } else if(path == ""){
+              type <- match.arg(type)
+              path_to_save <- paste0(type, "_wcm.csv")
+            } else {
+              type <- match.arg(type)
+              path_to_save <- file.path(path, paste0(type, "_wcm.csv"))
+
+            }
+
+            utils::write.csv2(data, file = path_to_save, ...)
+          })
