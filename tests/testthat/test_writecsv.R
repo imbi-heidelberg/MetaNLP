5785c01913727e418b30d4eb9392ed03d974beb7
unknown <brudernico@gmail.com>
2024-05-02 12:22:51 +0200

    update to 100% code coverage

--- tests/testthat/test_writecsv.R
+++ tests/testthat/test_writecsv.R
@@ -3,27 +3,34 @@ test_that("write_csv works correctly", {
   source_path  <- test_path("data", "test_data.csv")
   obj <- MetaNLP(source_path)
 
-  # test 1: default file name is correct
+  # test 1: when no path is specified, file is saved in working directory
+  write_csv(obj)
+
+  expect_true(
+    file.exists("train_wcm.csv")
+  )
+
+  # test 2: default file name is correct
   write_csv(obj, path = test_path("data"))
   source_path2 <- test_path("data", "train_wcm.csv")
   expect_no_error(
     read.csv2(source_path2)
   )
 
-  # test 2: file name can be changed
+  # test 3: file name can be changed
   write_csv(obj, path = test_path("data"), type = "test")
   source_path3 <- test_path("data", "test_wcm.csv")
   expect_no_error(
     read.csv2(source_path3)
   )
 
-  # test 3: correct data frame is saved
+  # test 4: correct data frame is saved
   data_read <- data.frame(read.csv2(source_path3))
   expect_true(
     all(obj@data_frame == data_read)
   )
 
-  # test 4: default file name can be overwritten
+  # test 5: default file name can be overwritten
   source_path4 <- test_path("data", "written.csv")
   write_csv(obj, source_path4)
 
@@ -31,6 +38,8 @@ test_that("write_csv works correctly", {
     read.csv2(source_path4)
   )
 
+  # remove all the files
+  file.remove("train_wcm.csv")
   file.remove(source_path2)
   file.remove(source_path3)
   file.remove(source_path4)
